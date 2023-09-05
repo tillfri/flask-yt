97d5715d55534901f0fe85ceb0a6d60001aaebfb
Till Fricke <till-fricke@gmx.de>
2023-09-05 21:35:14 +0200

    changed start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 docker run -d --name flask --mount type=bind,source=/home/till/PycharmProjects/flaskProject/downloads,target=/flaskYoutubeDownloader/downloads -p 5000:5000 flask-ytdownloader:latest
 
 # start container on PI
-# docker run -d --name flask --mount type=bind,source=/mnt/drive/stuff/Music,target=/flaskYoutubeDownloader/downloads -p 5000:5000 flask-ytdownloader:latest
+# docker run -d --name flask --mount type=bind,source=/mnt/drive/stuff/Music,target=/flaskYoutubeDownloader/downloads --mount type=bind,source=/home/till/flaskProject/data,target=/flaskYoutubeDownloader/instance -p 5000:5000 flask-ytdownloader:latest
 
 
 
